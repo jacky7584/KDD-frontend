5c9f9b9d75861f3996ca3d18b9fb0799103fb13a
Dex <44133270+jacky7584@users.noreply.github.com>
2023-10-13 15:07:26 +0800

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -23,7 +23,7 @@ export default function Error404() {
         <div className="container mx-auto items-center flex flex-wrap flex-row justify-center">
           <div className="w-full text-center md:w-6/12 px-12 md:px-4">
             <h2 className="font-semibold text-4xl text-slate-600">404</h2>
-            <p className="text-lg leading-relaxed mt-4 mb-4 text-slate-500">NOT FOUND</p>
+            <p className="text-lg leading-relaxed mt-4 mb-4 text-slate-500">NOT FOUND2</p>
           </div>
         </div>
       </section>
